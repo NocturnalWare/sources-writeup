316200e72a1e7f60374cc642a9893fce529d2b0f
Jota Vares <joe@eternallynocturnal.com.com>
2017-03-24 05:44:58 -0400

    master commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+<html lang="{{ config('app.locale') }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Shitsource</title>
+        <!-- Latest compiled and minified Bootstrap CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+        <!-- Optional theme -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #fff;
+                font-family: 'Raleway', sans-serif;
+                font-weight: 100;
+                height: 100vh;
+                margin: 0;
+            }
+
+            p {
+                font-size:1.3em;
+                color: #000;
+                font-weight: bold;
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+
+            .links > a {
+                color: #636b6f;
+                padding: 0 25px;
+                font-size: 12px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .m-b-md {
+                margin-bottom: 30px;
+            }
+        </style>
+    </head>
+    <body>
+    <div class="col-xs-12">
+        <h1 style="font-weight: bold">
+                How To Verify Shit Articles on the Internet
+        </h1>
+        <hr/>
+        <div class="col-xs-12">
+            <div class="col-xs-12 col-md-6 col-md-offset-3">
+                <center>
+                    <img class="img-responsive" src="https://s3.amazonaws.com/shitsource/independent_article_link.jpg">
+                    <br>
+                </center>
+                <h2 style="font-weight:bold;text-align:center">
+                    Supreme Court Rules In Favor of  SW GENERAL, INC.
+                    <hr/>
+                </h2>
+                <p>
+                    This article is pretty easy to verify because it refers to a Supreme Court decision, and those are <a href="https://www.supremecourt.gov/opinions/16pdf/15-1251_ed9g.pdf">published online for all to see.</a> The linked PDF from the court is what us elite snowflakes refer to as a “primary source” and the link above is what we would call a “tertiary source.” What that means is instead of being the ruling or referencing the ruling (secondary source), this article instead refers to the secondary source referencing the ruling. Not a good sign.
+                </p>
+                <br>
+                <div class="col-xs-12 col-md-6">
+                    <div class="row">
+                        <p>
+                            A quick look at the clickbait-as-fuck headlines on independentsentinel.com should be enough to send you to DEFCON 2. If it's not, the article is written by the widely known and very reputable internet journalist “S. Noble"
+                        </p>
+                    </div>
+                </div>
+                <div class="col-xs-12 col-md-6">
+                    <center>
+                        <img class="img-responsive" src="https://s3.amazonaws.com/shitsource/s_nobel.jpg">
+                    </center>
+                    <br>
+                </div>
+                <p>
+                    If you're still not convinced that this might not be a great place to get information, one need not look further than the lack of security certificate or appropriate corporate address information/privacy policy that even <a href="https://www.infowars.com/terms-of-service/">the shadiest of Russian propaganda outlets</a> manage to provide.
+                </p>
+                <br>
+                <div class="row">
+                    <center>
+                        <img class="img-responsive" src="https://s3.amazonaws.com/shitsource/ssl_cert.jpg">
+                    </center>
+                </div>
+                <div class="col-xs-12">
+                    <div class="row">
+                        <p>
+                            <br>
+                            Now that alarm bells are [hopefully] ringing, let's have a look at the secondary source this article references over at “freebeacon.com" ...Uh oh, red flag! there's no security certificate here either! But at least they have a privacy policy and actual authors. I wouldn't trust what I read here, but at least in this article they reference the primary source (sometimes the rabbit hole goes deep.)
+                        </p>
+                        <p>
+                            So now that you've got a better idea on how to qualify a source let's talk about the ruling and why the title of this "article" is inherently misleading. In the example, we're talking about United States Supreme Court ruling. In order to be heard by the Supreme Court, a case has to move through many lower courts in the federal circuit (US Constitution Article 3!) This means that the NLRB appointment wasn't an obvious violation of the constitution and required more legal clarification – the entire point of the US judicial system.
+                            <br>
+                            <br>
+                            At the end of the day, what we have here is a shitty opinion based on a true story.
+                            <br>
+                            <br>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </body>
+</html>
