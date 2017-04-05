6487b0f23b6eb899672974fd12fb02dffcabde12
Jota Vares <joe@eternallynocturnal.com.com>
2017-04-04 20:38:27 -0400

    moves header a bit

--- resources/views/layouts/topnav.blade.php
+++ resources/views/layouts/topnav.blade.php
@@ -9,7 +9,7 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="#"><b>Computer Poetry 101<b></a>
+      <a class="navbar-brand" style="padding-left:50px;" href="#"><b>Computer Poetry 101<b></a>
     </div>
 
     <!-- Collect the nav links, forms, and other content for toggling -->
